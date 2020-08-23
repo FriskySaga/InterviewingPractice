e26d4d2632a39675870502323f092b913543a2d9
FriskySaga <friskysaga@gmail.com>
2020-08-23 19:56:31 -0400

    I found my Havarti cheese

--- DesignPatterns/Factory/Cogs/CogFactory.py
+++ DesignPatterns/Factory/Cogs/CogFactory.py
@@ -6,6 +6,11 @@ from Cogs.CogEnums import SellbotCogType
 from Cogs.SellbotCogTypes.ColdCaller import ColdCaller
 from Cogs.SellbotCogTypes.Telemarketer import Telemarketer
 
+SELLBOT_COG_TYPES = {
+  SellbotCogType.COLD_CALLER:  ColdCaller,
+  SellbotCogType.TELEMARKETER: Telemarketer,
+}
+
 def makeCog(cogType):
   """Create a Sellbot cog.
   :param cogType: The type of cog to make
@@ -13,17 +18,8 @@ def makeCog(cogType):
   """
   # Create Sellbot cogs
   if isinstance(cogType, SellbotCogType):
+    return SELLBOT_COG_TYPES[cogType]()
 
-    if cogType == SellbotCogType.COLD_CALLER:
-      return ColdCaller()
-    
-    elif cogType == SellbotCogType.TELEMARKETER:
-      return Telemarketer()
-    
-    else:
-      logging.error(f'{cogType} is unsupported')
-      return NotImplemented
-  
   # Bossbot/Cashbot/Lawbot cogs are unsupported
   else:
     logging.error(f'{cogType} is unsupported')
