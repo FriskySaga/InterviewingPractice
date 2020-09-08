98034fae01065bb72e10a6fbf4b1e8b3441697a3
FriskySaga <friskysaga@gmail.com>
2020-09-07 23:59:17 -0400

    Simple template example

--- Languages/CPlusPlus/Templates.cpp
+++ Languages/CPlusPlus/Templates.cpp
@@ -2,3 +2,19 @@
  * C++14
  */
 
+#include <iostream>
+
+template <class T>
+T multiply(T a, T b)
+{
+  T result = a * b;
+  return result;
+}
+
+int main()
+{
+  std::cout << multiply(4, 5) << std::endl; // 20
+  std::cout << multiply(10.5, 2.0) << std::endl; // 21
+  return 0;
+}
+
