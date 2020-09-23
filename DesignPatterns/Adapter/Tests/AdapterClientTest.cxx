6a772e3c668f8df3fe8408037d299f77802c359e
FriskySaga <friskysaga@gmail.com>
2020-09-22 22:23:49 -0400

    Create Adapter folder

---
+++ DesignPatterns/Adapter/Tests/AdapterClientTest.cxx
@@ -0,0 +1,16 @@
+// External includes
+#include <gtest/gtest.h>
+
+// Project includes
+
+
+TEST(NinjaClientTest, StrategicalTest)
+{
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
