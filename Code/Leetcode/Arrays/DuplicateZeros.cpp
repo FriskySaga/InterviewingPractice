22cc3622f3e3a872c32fc040026a23f29be629d2
FriskySaga <friskysaga@gmail.com>
2020-10-30 20:08:06 -0400

    Optimal solution to Leetcode Medium

--- Code/Leetcode/Arrays/DuplicateZeros.cpp
+++ Code/Leetcode/Arrays/DuplicateZeros.cpp
@@ -26,6 +26,7 @@
  */
 
 #include <cassert>
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
@@ -33,26 +34,61 @@ using namespace std;
 
 void duplicateZeros(vector<int>& arr)
 {
-  vector<int> brr;
-  for (auto& iter : arr)
+  uint16_t possibleDuplicates = 0;
+  int maxOrigIndex = arr.size() - 1;
+
+  // Identify the number of duplicate zeros to keep
+  // Ex: 1 0 2 3 0 4
+  // Ex:               maxOrigIndex - possibleDuplicates
+  // index 0, value 1: 5 - 0
+  // index 1, value 0: 5 - 0
+  // index 2, value 2: 5 - 1
+  // index 3, value 3: 5 - 1
+  // index 4, value 0: 5 - 1
+  // index 5, value 4: 5 - 2 (don't enter loop)
+  for (uint16_t i = 0; i <= maxOrigIndex - possibleDuplicates; ++i)
   {
-    if (brr.size() < arr.size())
+    if (arr[i] == 0)
     {
-      if (iter != 0)
+      // Don't duplicate this zero since duplicating would go beyond the array
+      // Ex: the second 0
+      // Ex: 4 == 5 - 1
+      if (i == maxOrigIndex - possibleDuplicates)
       {
-        brr.push_back(iter);
+        // Immediately copy this 0
+        arr[maxOrigIndex] = 0;
+        --maxOrigIndex;
+        break;
       }
-      else
-      {
-        brr.push_back(iter);
-        if (brr.size() < arr.size())
-        {
-          brr.push_back(0);
-        }
-      }
+
+      // Ex: the first 0, increment counter to 1
+      ++possibleDuplicates;
     }
   }
-  arr = brr;
+
+  // Ex: lastIdx = 4 - 1 = 3
+  int lastIdx = maxOrigIndex - possibleDuplicates;
+
+  // Modify the original array with the duplicated zeros
+  // Ex: 1 0 2 3 0 4
+  // index 3, value 3, arr[4] = arr[3], dupes 1
+  // index 2, value 2, arr[3] = arr[2], dupes 1
+  // index 1, value 0, arr[2] = arr[1], dupes 1
+  // index 1, value 0, arr[1] = arr[1], dupes 0
+  // index 0, value 1, arr[0] = arr[0], dupes 0
+  for (int16_t i = lastIdx; i >= 0; --i)
+  {
+    if (arr[i] == 0)
+    {
+      arr[i+possibleDuplicates] = 0;
+      --possibleDuplicates;
+      arr[i+possibleDuplicates] = 0;
+    }
+    else
+    {
+      arr[i+possibleDuplicates] = arr[i];
+    }
+  }
 }
 
 int main()
