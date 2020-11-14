1808ac71e8d621edfc359de20fe514109db3a593
FriskySaga <friskysaga@gmail.com>
2020-11-13 19:01:22 -0500

    Firecode solution with a regular STL map

--- Code/Firecode/FirstNonDuplicateCharacter.cpp
+++ Code/Firecode/FirstNonDuplicateCharacter.cpp
@@ -11,26 +11,58 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-// char firstNonRepeating(string str)
-// {
-//   unordered_map<char> 
+/**
+ * 1. Create the character count map < char, int > hmp.
+ * For each character, if there is no value stored in the map, insert an entry hmp.insert(pair(c, 1)) .
+ * Else find the entry using iterators it = hmp.find(c) and increment the value of  it->second by 1.
+ * 
+ * 2. Scan the string, return the character if the count in the hmp is 1 .
+ * If no characters have count of 1 , return '0'
+ */
+char firstNonRepeating(string str)
+{
+  map <char, int> hmp;
 
-//   for (auto iter = str.cbegin(); iter != str.cend(); ++iter)
-//   {
-    
-//   }
-//   return '0';
-// }
+  // Create the character count map
+  for (auto c = str.cbegin(); c != str.cend(); ++c)
+  {
+    auto findResult = hmp.find(*c);
+
+    // Add character to the map
+    if (findResult == hmp.end())
+    {
+      hmp.insert(pair<char, int>(*c, 1));
+    }
+
+    // Else, character already exists in the map, increase the count
+    else
+    {
+      ++findResult->second;
+    }
+  }
+
+  // Find the first character with a count of 1
+  for (auto c = str.cbegin(); c != str.cend(); ++c)
+  {
+    if (hmp.find(*c)->second == 1)
+    {
+      return *c;
+    }
+  }
+
+  return '0';
+}
 
 int main()
 {
   assert(firstNonRepeating("abcdcd") == 'a');
-  // assert(firstNonRepeating("cbcd") == 'b');
-  // assert(firstNonRepeating("cdcd") == '0');
-  // assert(firstNonRepeating("") == '0');
+  assert(firstNonRepeating("cbcd") == 'b');
+  assert(firstNonRepeating("cdcd") == '0');
+  assert(firstNonRepeating("") == '0');
 
   return 0;
 }
