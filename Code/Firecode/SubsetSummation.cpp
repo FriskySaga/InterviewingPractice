3508fcc76863ca5c6e66e24d2e3f86242df3da2c
FriskySaga <friskysaga@gmail.com>
2021-01-23 13:16:20 -0500

    Add alternate solutions and stylize all solutions

--- Code/Firecode/SubsetSummation.cpp
+++ Code/Firecode/SubsetSummation.cpp
@@ -10,24 +10,60 @@
 // Examples:
 // group_sum({1,2,3,6,5},5,10) ==> true
 // group_sum({1,2,3,6,5},5,18) ==> false
-bool find_sum(int startindx, int arr[], int sz, int target)
+bool find_sum(int startIndex, int arr[], int sz, int target)
 {
-    if (target == 0)            
-      return true;
-    if (startindx >= sz)        /* Return when read the entire array */
-      return false;
-    /* Calculate the sum by including the element at startindx */
-    if (find_sum(startindx+1, arr, sz, target-arr[startindx]))
-      return true;
-    else   /* Calculate the sum without the element at startindx */
-      return find_sum(startindx+1, arr, sz, target);
+  if (target == 0)            
+    return true;
+
+  // Return when read the entire array
+  if (startIndex >= sz)
+    return false;
+
+  // Calculate the sum by including the element at startIndex
+  if (find_sum(startIndex+1, arr, sz, target-arr[startIndex]))
+    return true;
+
+  // Calculate the sum without the element at startIndex
+  else
+    return find_sum(startIndex+1, arr, sz, target);
 }
 
 bool group_sum(int arr[], int size, int target)
 {
-    if (size <= 0) 
-      return 0;
-    return find_sum(0, arr, size, target);
+  if (size <= 0) 
+    return 0;
+  return find_sum(0, arr, size, target);
+}
+
+
+#include <vector>
+// Dynamic programming solution on O(size*target)
+// Doesn't this go out of bounds if any element in arr is larger than the target?
+bool group_sum(int arr[], int size, int target)
+{
+  if (size == 0)
+    return false;
+  
+  std::vector<bool> dp(target + 1, false);
+  dp[0] = true;
+  
+  for (std::vector<bool>::size_type i = 0; i < size; ++i)
+  {
+    for (std::vector<bool>::size_type k = target; k > 0; --k)
+    {
+        dp[k] = dp[k] || (k >= arr[i] && dp[k - arr[i]]);
+    }
+  }
+  return dp[target];
+}
+
+// Exponential time complexity
+bool group_sum(int arr[], int size, int target)
+{
+  if (target == 0) return true;
+  if (size <= 0) return false;
+  return group_sum(arr, size-1, target)
+         || group_sum(arr, size-1, target - arr[size-1]);
 }
 
 // Same problem as above, but with one extra criteria:
