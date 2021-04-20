f1d45c57e9e644267e5d7d8cf889ef540772121e
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-04-19 20:19:20 -0400

    Get rid of heavy PDFs

---
+++ Code/HackerRank/Bash/Easy/A_Personalized_Echo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Write a Bash script which accepts name as input
+# and display the greeting: "Welcome (name)"
+read name
+echo Welcome $name
+
--- Code/HackerRank/Bash/Easy/LoopingWithNumbers/Looping_With_Numbers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for x in {1..50}
-do
-  echo $x
-done
