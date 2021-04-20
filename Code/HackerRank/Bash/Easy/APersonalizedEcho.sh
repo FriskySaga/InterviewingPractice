0ef725c070185b9cdfbc88d76f2be165eb9b086d
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-04-19 20:38:01 -0400

    Polish scripts beyond the HackerRank statements

--- Code/HackerRank/Bash/Easy/APersonalizedEcho.sh
+++ Code/HackerRank/Bash/Easy/APersonalizedEcho.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 # Write a Bash script which accepts name as input
 # and display the greeting: "Welcome (name)"
-read name
-echo Welcome $name
+
+# Prompt for user's name
+read -p "Your name: " name
+echo "Welcome $name"
 
--- Code/HackerRank/Bash/Easy/LoopingWithNumbers.sh
+++ Code/HackerRank/Bash/Easy/LoopingWithNumbers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 # Use for loops to display the natural numbers from 1 to 50
+
 for x in {1..50}
 do
   echo $x
