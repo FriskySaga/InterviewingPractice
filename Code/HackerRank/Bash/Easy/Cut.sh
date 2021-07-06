362b9d384246d10793b6a023a9185d2f6a7846d3
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-07-06 12:36:44 -0400

    Hehe I know how to combine -d with -f for cut commands now!

--- Code/HackerRank/Bash/Easy/Cut.sh
+++ Code/HackerRank/Bash/Easy/Cut.sh
@@ -1,5 +1,6 @@
 # Given n lines of input, print the 3rd character from each line as a new line
-# of output. It is guaranteed that each line of input will have a 3rd character
+# of output. It is guaranteed that each line of input will have a 3rd
+# character
 cut -c3 $1
 
 # Display the 2nd and 7th character from each line of text
@@ -12,9 +13,14 @@ cut -c2-7 $1
 # Display the first four characters from each line of text
 cut -c-4 $1
 
-# Given a tab delimited file with several columns (tsv format), print the first
-# three fields
+# Given a tab delimited file with several columns (tsv format), print the
+# first three fields
 cut -f1-3 $1
 
 # Print the characters from the 13th position to the end
 cut -c13- $1
+
+# Given a sentence, identity and display its fourth word. Assume that the
+# space character is the only delimiter between words
+cut -d ' ' -f4 $1
+
