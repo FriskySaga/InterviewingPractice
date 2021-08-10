4fc41f1bf130e7da9d580d7db15c3b3d8b8e3381
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-08-09 22:02:52 -0400

    Template return placeholder

--- ProjectEuler/ProjectEuler100.py
+++ ProjectEuler/ProjectEuler100.py
@@ -30,6 +30,8 @@ def solveProblem2() -> int:
 
   By considering the terms in the Fibonacci sequence whose values do not exceed four
   million, find the sum of the even-valued terms.
+
+  :return: None
   """
   return None
 
